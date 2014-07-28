321f9ad4e0ea3c3a975a94999f9b15508f967a39
OHTA Shogo <athos0220@gmail.com>
2014-07-28 11:27:12 +0900

    change project.clj descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject symbol-analyzer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure code analyzer that tells us how each symbol is being used in the code"
+  :url "https://github.com/athos/symbol-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
