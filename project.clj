a64ddf7b9a18455ec792ab6aced2c5b90b554265
OHTA Shogo <athos0220@gmail.com>
2014-07-28 11:27:39 +0900

    add dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/athos/symbol-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]
+                 [org.clojure/core.match "0.2.1"]])
