5cb9500681ba9d44d2895d51de652e9ff0d549bb
OHTA Shogo <athos0220@gmail.com>
2014-08-06 17:20:37 +0900

    add another interface of analyzer targeted on sexps, for #12

--- src/symbol_analyzer/core.clj
+++ src/symbol_analyzer/core.clj
@@ -1,5 +1,6 @@
 (ns symbol-analyzer.core
-  (:require [symbol-analyzer.conversion :refer [convert]]
+  (:require [clojure.walk :refer [postwalk]]
+            [symbol-analyzer.conversion :refer [convert]]
             [symbol-analyzer.extraction :refer [extract]])
   (:import net.cgrand.parsley.Node))
 
@@ -8,6 +9,25 @@
     (fn []
       (swap! n inc)
       @n)))
+
+(defn- add-meta [x m]
+  (if (meta x)
+    (vary-meta x conj m)
+    (with-meta x m)))
+
+(defn- mark-sexp [sexp]
+  (-> (fn [t]
+        (if (symbol? t)
+          (add-meta t {:id (new-id)})
+          t))
+      (postwalk sexp)))
+
+(defn- annotate-sexp [sexp info]
+  (-> (fn [t]
+        (if-let [usage (and (symbol? t) (info (:id (meta t))))]
+          (add-meta t {:usage usage})
+          t))
+      (postwalk sexp)))
 
 (defn- postwalk-nodes [f node]
   (if (instance? Node node)
@@ -31,8 +51,15 @@
       (postwalk-nodes node)))
 
 ;;
-;; Entry point
+;; Entry points
 ;;
+
+(defn analyze-sexp [sexp & {:keys [ns]}]
+  (let [ns (or ns *ns*)
+        sexp (mark-sexp sexp)
+        info (extract sexp :ns ns :symbol-key :id)]
+    (annotate-sexp sexp info)))
+
 (defn analyze [root & {:keys [ns suppress-eval?]}]
   (let [ns (or ns *ns*)
         root (mark root)
