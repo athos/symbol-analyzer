98b5df22c38c03aab3ca4d9e2afd7c7ad8c85c13
OHTA Shogo <athos0220@gmail.com>
2014-09-09 15:57:23 +0900

    update Clojure version to 1.6.0, for #19

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/athos/symbol-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.trptcolin/sjacket "0.1.4" :exclusions [[org.clojure/clojure]]]
                  [org.clojure/core.match "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5"]]
